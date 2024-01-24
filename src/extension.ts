3db06863ec0aa3be746fe91fde03c8459e0090bd
Anthony Delre <anthonydelre@outlook.com>
2024-01-24 02:12:49 -0500

    Added extension settings

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,12 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
+import { WorkspaceConfiguration } from "vscode";
+
+enum FILE_TYPES {
+  COMMAND = "Command",
+  SUBSYSTEM = "Subsystem",
+}
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -8,98 +14,139 @@ export function activate(context: vscode.ExtensionContext) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
+
   let disposable1 = vscode.commands.registerCommand(
-    "frc-2024.generateSubsystem",
-    async () => {
-      const subsystemName = await vscode.window.showInputBox({
-        placeHolder: "Subsystem Name",
-      });
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-
-      const wsedit = new vscode.WorkspaceEdit();
-      if (vscode.workspace.workspaceFolders) {
-        const fs = require("node:fs");
-        const wsPath = vscode.workspace.workspaceFolders[0].uri.fsPath; // gets the path of the first workspace folder
-        const SubsystemDIR = "/src/subsystems/";
-        const filePath = vscode.Uri.file(
-          wsPath + SubsystemDIR + subsystemName + ".java"
-        );
-        vscode.window.showInformationMessage(wsPath);
-        wsedit.createFile(filePath, { ignoreIfExists: true });
-        await vscode.workspace.applyEdit(wsedit);
-
-        const content =
-          `package ${SubsystemDIR};\n\n` +
-          "import edu.wpi.first.wpilibj2.command.SubsystemBase;\n\n" +
-          `public class ${subsystemName} extends SubsystemBase{\n\tpublic ${subsystemName}(){\n\n\t}\n\t@Override\n\tpublic void periodic() {\n\t}\n}`;
-
-        try {
-          fs.writeFileSync(filePath.path, content);
-          // file written successfully
-        } catch (err) {
-          console.error(err);
-          if (err instanceof Error) {
-            vscode.window.showInformationMessage(err.message);
-          }
-        }
-        vscode.window.showInformationMessage(
-          `Subsystem ${subsystemName} has been generated!`
-        );
-      } else {
-        vscode.window.showInformationMessage("WorkspaceFolders is null!");
-      }
-    }
-  );
-
-  
-let disposable2 = vscode.commands.registerCommand(
     "frc-2024.generateCommand",
-    async () => {
-      const commandName = await vscode.window.showInputBox({
-        placeHolder: "Command Name",
-      });
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-
-      const wsedit = new vscode.WorkspaceEdit();
-      if (vscode.workspace.workspaceFolders) {
-        const fs = require("node:fs");
-        const wsPath = vscode.workspace.workspaceFolders[0].uri.fsPath; // gets the path of the first workspace folder
-        const CommandDIR = "/src/commands/";
-        const filePath = vscode.Uri.file(
-          wsPath + CommandDIR + commandName + ".java"
-        );
-        vscode.window.showInformationMessage(wsPath);
-        wsedit.createFile(filePath, { ignoreIfExists: true });
-        await vscode.workspace.applyEdit(wsedit);
-
-        const content =
-          `package ${CommandDIR};\n\n` +
-          "import edu.wpi.first.wpilibj2.command.CommandBase;\n\n" +
-          `public class ${commandName} extends CommandBase{\n\tpublic ${commandName}(){\n\n\t}\n}`;
-
-        try {
-          fs.writeFileSync(filePath.path, content);
-          // file written successfully
-        } catch (err) {
-          console.error(err);
-          if (err instanceof Error) {
-            vscode.window.showInformationMessage(err.message);
-          }
-        }
-        vscode.window.showInformationMessage(
-          `Command ${commandName} has been generated!`
-        );
-      } else {
-        vscode.window.showInformationMessage("WorkspaceFolders is null!");
-      }
+    () => {
+      prepareFile(FILE_TYPES.COMMAND);
     }
   );
-  
+
+  let disposable2 = vscode.commands.registerCommand(
+    "frc-2024.generateSubsystem",
+    () => {
+      prepareFile(FILE_TYPES.SUBSYSTEM);
+    }
+  );
+
   context.subscriptions.push(disposable1);
   context.subscriptions.push(disposable2);
+}
 
+async function prepareFile(fileType: FILE_TYPES) {
+  if (!vscode.workspace.workspaceFolders) {
+    vscode.window.showInformationMessage("Please select a workspace!");
+    return;
+  }
+
+  const config = vscode.workspace.getConfiguration("FRC");
+  const fileName = await vscode.window
+    .showInputBox({
+      placeHolder: `${fileType} Name`,
+    })
+    .then((value) => capitalizeFirstLetter(value));
+
+  if (fileName === undefined) {
+    vscode.window.showInformationMessage(
+      `Could not parse value ${fileName}, please enter a value.`
+    );
+    return;
+  }
+
+  const subName = fileName?.split(/(?=[A-Z])/);
+
+  if (fileType === FILE_TYPES.COMMAND) {
+    if (subName.length < 2) {
+      vscode.window.showInformationMessage(
+        `Could not parse value ${fileName}, please use camel case formatting.`
+      );
+      return;
+    }
+    prepareCommandFile(fileName, config);
+  }
+
+  prepareSubsystemFile(subName[0], config);
+}
+
+function prepareCommandFile(
+  fileName: string | undefined,
+  config: WorkspaceConfiguration
+) {
+  const fileDIR = config.get(
+    `directory.${FILE_TYPES.COMMAND.toLowerCase()}s`
+  ) as string;
+  const fileDIRSanitized = fileDIR.slice(-1) === "/" ? fileDIR : fileDIR + "/";
+  const content =
+    `package ${fileDIRSanitized
+      .substring(1, fileDIRSanitized.lastIndexOf("/"))
+      .replaceAll("/", ".")};\n\n` +
+    `import edu.wpi.first.wpilibj2.command.${FILE_TYPES.COMMAND}Base;\n\n` +
+    `public class ${fileName} extends CommandBase{\n\tpublic ${fileName}(){\n\n\t}\n}`;
+
+  createFile(FILE_TYPES.COMMAND, fileName, fileDIRSanitized, content);
+}
+
+async function prepareSubsystemFile(
+  fileName: string | undefined,
+  config: WorkspaceConfiguration
+) {
+  const fileDIR = config.get(
+    `directory.${FILE_TYPES.SUBSYSTEM.toLowerCase()}s`
+  ) as string;
+  const fileDIRSanitized = fileDIR.slice(-1) === "/" ? fileDIR : fileDIR + "/";
+  const content =
+    `package ${fileDIRSanitized
+      .substring(1, fileDIRSanitized.lastIndexOf("/"))
+      .replaceAll("/", ".")};\n\n` +
+    `import edu.wpi.first.wpilibj2.command.${FILE_TYPES.SUBSYSTEM}Base;\n\n` +
+    `public class ${fileName} extends SubsystemBase{\n\tpublic ${fileName}(){\n\n\t}\n\t@Override\n\tpublic void periodic() {\n\t}\n}`;
+
+  createFile(FILE_TYPES.SUBSYSTEM, fileName, fileDIRSanitized, content);
+}
+
+async function createFile(
+  fileType: string,
+  fileName: string | undefined,
+  fileDIR: string,
+  content: string
+) {
+  if (vscode.workspace.workspaceFolders) {
+    const wsedit = new vscode.WorkspaceEdit();
+    const fs = require("node:fs");
+    const wsPath = vscode.workspace.workspaceFolders[0].uri.fsPath;
+    const file = vscode.Uri.file(wsPath + fileDIR + fileName + ".java");
+
+    vscode.window.showInformationMessage("lastchar: " + fileDIR.slice(-1));
+
+    if (fs.existsSync(file.path)) {
+      vscode.window.showInformationMessage(
+        `${fileType} ${fileName} already exists!`
+      );
+      return;
+    }
+
+    wsedit.createFile(file, { ignoreIfExists: true });
+    await vscode.workspace.applyEdit(wsedit);
+
+    try {
+      fs.writeFileSync(file.path, content);
+      // file written successfully
+      vscode.window.showInformationMessage(
+        `${fileType} ${fileName} has been generated!`
+      );
+    } catch (err) {
+      console.error(err);
+      if (err instanceof Error) {
+        vscode.window.showInformationMessage(err.message);
+      }
+    }
+  } else {
+    vscode.window.showInformationMessage("WorkspaceFolders is null!");
+  }
+}
+
+function capitalizeFirstLetter(val: string | undefined): string | undefined {
+  return val ? val?.charAt(0).toUpperCase() + val?.slice(1) : undefined;
 }
 
 // This method is called when your extension is deactivated
